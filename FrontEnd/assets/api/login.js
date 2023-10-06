dd5cd6f6359bfccc947598248b2228eb499899f1
Martins-Anthony <thony.martins.67@gmail.com>
2023-10-06 10:40:29 +0200

    modif addEventListener("change")

--- FrontEnd/assets/api/login.js
+++ FrontEnd/assets/api/login.js
@@ -1,56 +1,43 @@
 const idSend = document.getElementById("connection")
-const classError = document.querySelector("error")
-let displayCheckerEmailError = true
-let displayCheckerPasswordError = true
-
-function checkEmail(email) {
-    const emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+")
-    const tagEmail = document.getElementById("email")
-    const emailError = document.getElementById("email-error")
-
-    if (emailRegExp.test(email)) {
-        if (displayCheckerEmailError === false) {
-            emailError.remove()
-        }
-        displayCheckerEmailError = true
-    } else {
-        if (displayCheckerEmailError) {
-            tagEmail.insertAdjacentHTML("afterend", '<p class="error" id="email-error">E-mail incorrect</p>')
-            displayCheckerEmailError = false
+class userInfo {
+    constructor(name,regExp,displayCheck) {
+        this.name = name
+        this.regExp = regExp
+        this.displayCheck = displayCheck
+        this.tag = document.getElementById(this.name)     
+    }
+    initializeTag() {
+        if (!this.displayCheck) {
+            this.tag.insertAdjacentHTML("afterend",`<p class="error" id="${this.name}-error">${this.name.charAt(0).toUpperCase()+this.name.slice(1)} incorrect</p>`)            
+            return this.displayCheck = true       
         }
     }
 }
 
-function checkPassword(password) {
-    const passwordRegExp = new RegExp("^((?=\\S*?[A-Z])(?=\\S*?[a-z])(?=\\S*?[0-9]).{5,})\\S$")
-    const tagPassword = document.getElementById("password")
-    const passwordError = document.getElementById("password-error")
-
-    if (passwordRegExp.test(password)) {
-        if (displayCheckerPasswordError === false) {
-            passwordError.remove()
-        }
-        displayCheckerPasswordError = true
-    } else {
-        if (displayCheckerPasswordError) {
-            tagPassword.insertAdjacentHTML("afterend", '<p class="error" id="password-error">Mot de passe incorrect au moins 6 caractères une majuscule, une minuscule et un caractère numérique</p>')
-            displayCheckerPasswordError = false
-        }
-    }  
-}
+const userInfoTable = [
+    userInfoEmail = new userInfo("e-mail", new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+"),false),
+    userInfoPassword = new userInfo("passwords", new RegExp("^((?=\\S*?[A-Z])(?=\\S*?[a-z])(?=\\S*?[0-9]).{5,})\\S$"),false)
+]
 
 idSend.addEventListener("change", () => {
-    const tagEmail = document.getElementById("email").value
-    const tagPassword = document.getElementById("password").value
+    for (let i = 0; i < userInfoTable.length; i++) {
+        const tagValue = userInfoTable[i].tag.value
+ 
+        if (tagValue !== "") {             
+            if (!userInfoTable[i].regExp.test(tagValue)){
+                userInfoTable[i].initializeTag()
+            }
+            else if (userInfoTable[i].regExp.test(tagValue)){ 
+                if (userInfoTable[i].displayCheck) {
+                    const tagSelectRemove = document.getElementById(`${userInfoTable[i].name}-error`).remove()
 
-    if (tagEmail != "") {
-        checkEmail(tagEmail)
-    } 
-    if (tagPassword != "") {
-        checkPassword(tagPassword)
-    } 
+                    userInfoTable[i].displayCheck = false
+                    return tagSelectRemove
+                }                      
+            } 
+        }  
+    }
 })
-
 
 idSend.addEventListener("submit", async (event) => {
     event.preventDefault()
@@ -62,19 +49,18 @@ idSend.addEventListener("submit", async (event) => {
         
     const payload = JSON.stringify(id)
 
-    try {
-        const response = await fetch("http://localhost:5678/api/users/login", {
-            method: "POST",
-            headers: { "content-Type": "application/json"},
-            body: payload
-        })
+    fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { "content-Type": "application/json"},
+        body: payload
+    }).then(response => {
         if (response.ok) {
             window.location.href = "./index.html"
+        } else {
+            const displayError = document.getElementById("passwords")
+            displayError.insertAdjacentHTML("afterend",`<p class="error">E-mail ou Mot de passe incorrect</p>`)
         }
-
-    } catch (error) {
-            console.error("Une erreur s'est produite :", error)
-    }
-        
+    })
+    
 })
 
